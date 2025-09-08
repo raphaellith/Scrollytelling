8b1ff8c03caa94175f70a631152492d925512008
raphaellith <raphaellith@gmail.com>
2025-09-08 23:30:42 +0800

    completed basic scrollytelling mechanism

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,15 @@
-const waypoint = new Waypoint({
-    element: document.getElementById('fig'),
-    handler: function () {
-    }
+const steps = Array.from(document.querySelectorAll(".step"));
+const fig = document.getElementById("fig");
+steps.forEach(step => {
+    const stepID = parseInt(step.getAttribute("stepId"));
+    return new Waypoint({
+        element: step,
+        handler: function (direction) {
+            const nextStep = direction === 'down' ? stepID : Math.max(0, stepID - 1);
+            fig.innerText = nextStep.toString();
+        },
+        offset: "50%",
+    });
 });
 export {};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-const waypoint = new Waypoint({
-  element: document.getElementById('fig')!,
-  handler: function() {
-  }
-})
+const steps = Array.from(document.querySelectorAll(".step")) as HTMLElement[];
+const fig = document.getElementById("fig")!;
+
+steps.forEach(step => {
+    const stepID: number = parseInt(step.getAttribute("stepId")!);
+
+    return new Waypoint({
+		element: step,
+		handler: function(direction) {
+			const nextStep = direction === 'down' ? stepID : Math.max(0, stepID - 1)
+			fig.innerText = nextStep.toString();
+		},
+		offset: "50%",
+	})
+});
