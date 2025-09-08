29999fe41804ff5896d2972e041296dec3c6acd1
raphaellith <raphaellith@gmail.com>
2025-09-08 17:27:43 +0800

    installed and imported waypoints

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,2 @@
-export {};
+import 'waypoints';
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,4 @@
-// https://stackoverflow.com/questions/58182148/can-intersection-observer-be-used-to-calculate-trigger-at-specific-offset-top-of
-const options = {
-    root: null, // Document's viewport
-    rootMargin: "-200px 0px -200px 0px",
-    scrollMargin: "0px",
-    threshold: 1,
-};
-function callback(entries, observer) {
-    entries.forEach((entry) => {
-        console.log(entry);
-        if (!entry.isIntersecting) {
-            document.querySelector("#fig").textContent = entry.target.id;
-        }
-    });
-}
-const observer = new IntersectionObserver(callback, options);
-// observer.observe(document.querySelector(".step"));
-document.querySelectorAll(".step").forEach(t => observer.observe(t));
-// observer.observe();
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("waypoints");
+//# sourceMappingURL=index.js.map
