497e82d390c8cce2142201914dbc2b883d017637
raphaellith <raphaellith@gmail.com>
2025-09-09 11:38:36 +0800

    removed unnecessary css; organised and commented on code; added more lorem ipsum

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
+// Access DOM elements
 const steps = Array.from(document.querySelectorAll(".step")) as HTMLElement[];
-const fig = document.getElementById("fig")!;
+const graphic: HTMLElement = document.getElementById("graphic")!;
 
+// The sticky offset will be read from CSS
+const stickyOffset: string = graphic.style.top;
+
+// Create a Waypoint object that updates the graphic when scrolling past each step
 steps.forEach(step => {
-    const stepID: number = parseInt(step.getAttribute("stepId")!);
+    const stepID: number = parseInt(step.getAttribute("step-id")!);
 
     return new Waypoint({
 		element: step,
 		handler: function(direction) {
 			const nextStep = direction === 'down' ? stepID : Math.max(0, stepID - 1)
-			fig.innerText = nextStep.toString();
+			graphic.innerText = nextStep.toString();
 		},
-		offset: "50%",
+		offset: stickyOffset,
 	})
 });
